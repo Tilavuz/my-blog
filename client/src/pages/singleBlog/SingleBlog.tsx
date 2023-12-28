532762874fc60f6a3695fdbab851c16f03179fda
tilav <tilav@tilav.tilav>
2023-12-28 17:21:10 +0500

    Frontend da singleBlog ni tugatdim

--- client/src/pages/singleBlog/SingleBlog.tsx
+++ client/src/pages/singleBlog/SingleBlog.tsx
@@ -19,16 +19,16 @@ function SingleBlog() {
     }, [])
 
   return (
-    <div className='flex flex-col gap-4 h-screen py-20 px-2'>
+    <div className='flex flex-col gap-4 h-screen py-20 px-2 md:py-24 lg:container lg:mx-auto lg:py-32'>
         {
           blog && (
             <>
               <div>
-                <p className='font-bold text-xl'>Author: <span className='font-thin capitalize'>{blog.author}</span></p>
-                <p>{blog.title}</p>
+                <p className='font-bold text-xl lg:text-2xl'>Author: <span className='font-thin capitalize'>{blog.author}</span></p>
+                <p className='font-bold text-xl lg:text-2xl'>Tittle: <span className='font-thin'>{blog.title}</span></p>
               </div>
-              <img className='rounded-lg' src={`http://localhost:3000/${blog.image}`} alt={blog.title} />
-              <p>{blog.desc}</p>
+              <img className='sm:w-96 md:w-1/2 xl:w-5/12 rounded-lg' src={`http://localhost:3000/${blog.image}`} alt={blog.title} />
+              <p className='lg:text-xl'>{blog.desc}</p>
             </>
           )
         }
